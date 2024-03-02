374659f560dff29aa4c493ac06b7a89ba1b27925
Yadlapati Siva Sai Maruthi Charan <119681925+MaruthiCharan2403@users.noreply.github.com>
2024-03-02 21:11:40 +0530

    Update guestUpload.js

--- Express/routes/guestUpload.js
+++ Express/routes/guestUpload.js
@@ -31,7 +31,7 @@ router.post('/guestUp', Guestupload.any(), async (req, res) => {
       });
       try {
         const guestFlaskResponse = [];
-        const guestResponse_flask = await axios.post("https://tohflask.onrender.com/classify", guestFlaskRequestData);
+        const guestResponse_flask = await axios.post("https://finalflask.el.r.appspot.com/classify", guestFlaskRequestData);
         for (let i = 0; i < files.length; i++) {
           guestFlaskResponse.push(guestResponse_flask.data[i]);
         }

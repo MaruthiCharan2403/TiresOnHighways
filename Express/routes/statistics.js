6636dbbdeedd540d8f80f94ff5702e1982522265
Yadlapati Siva Sai Maruthi Charan <119681925+MaruthiCharan2403@users.noreply.github.com>
2024-03-02 21:31:34 +0530

    Update statistics.js

--- Express/routes/statistics.js
+++ Express/routes/statistics.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const TollData = require('../models/TollDataSch');
 const cors = require('cors');
 router.use(cors({
-    origin: 'https://frontend-black-eight.vercel.app/',
+    origin: 'https://frontend-black-eight.vercel.app',
     credentials: true,
 }));
 
